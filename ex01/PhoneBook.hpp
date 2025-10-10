edfa438f50869a898a34f4fc9b6ad730d5801ca5
Muhammad Bani Yamin <mbani-ya@u91z02s01.42kl.edu.my>
2025-10-10 18:26:11 +0800

    need to study

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,9 +6,31 @@
 /*   By: mbani-ya <mbani-ya@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 15:40:34 by mbani-ya          #+#    #+#             */
-/*   Updated: 2025/10/09 18:01:38 by mbani-ya         ###   ########.fr       */
+/*   Updated: 2025/10/10 17:58:38 by mbani-ya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <>
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include <iostream>
+# include <string>
+
+class Contact {
+private:
+	std::string firstName;
+	std::string lastName;
+	std::string nickName;
+	std::string phoneNumber;
+	std::string darkestSecret;
+
+public:
+	void setContact(std::string firstName, std::string lastName,
+					std::string nickname, std::string phoneNumber,
+					std::string darkestSecret);
+	void displaySummary(int index) const;
+	void displayFull() const;
+	bool isEmpty() const;
+}
+
+#endif
